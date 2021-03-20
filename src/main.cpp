8c95540bfe0e56cc07b928431623443433862d58
Bricktricker <philipphuettig@arcor.de>
2021-03-20 14:49:38 +0100

    fix openMP linking

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,16 @@
 
 #define PARENT_TID 0
 
-int main() {
-int tid;
-  #pragma omp parallel
-  {
-    tid = omp_get_thread_num();
-    if(tid == PARENT_TID)
-      printf("Parent: %d threads running\n", omp_get_num_threads());
-    else
-      printf("Thread# %d: Hello world\n", tid);
-  }
-  return EXIT_SUCCESS;
+int main()
+{
+    int tid;
+#pragma omp parallel
+    {
+        tid = omp_get_thread_num();
+        if (tid == PARENT_TID)
+            printf("Parent: %d threads running\n", omp_get_num_threads());
+        else
+            printf("Thread# %d: Hello world\n", tid);
+    }
+    return EXIT_SUCCESS;
 }
