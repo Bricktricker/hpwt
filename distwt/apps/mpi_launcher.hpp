5f0713a1901c115ea443fac686371dc3c23a5b96
Bricktricker <philipphuettig@arcor.de>
2021-05-06 14:18:06 +0200

    use mpi_launcher class to parse cli args

---
+++ distwt/apps/mpi_launcher.hpp
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <tlx/cmdline_parser.hpp>
+#include <distwt/mpi/context.hpp>
+
+#include <distwt/mpi/uint_types.hpp>
+#include <src/validate_distwt.hpp>
+
+template<typename mpi_app_t>
+int mpi_launch(int argc, char** argv) {
+    // Read command-line
+    tlx::CmdlineParser cp;
+
+    size_t rdbufsize = 0; // default to local input size
+    cp.add_bytes('r', "rbuf", rdbufsize, "File read buffer size.");
+
+    std::string output("");
+    cp.add_string('o', "output", output, "Name of output file.");
+
+    size_t prefix = SIZE_MAX; // default to whole file
+    cp.add_bytes('p', "prefix", prefix, "Only process prefix of input file.");
+
+    size_t sym_width = 1;
+    cp.add_bytes('w', "width", sym_width, "Number of bytes per input symbol.");
+
+    bool eff_input = false;
+    cp.add_flag('e', "effective", eff_input,
+        "Input is already an effective transform (skip histogram computation).");
+
+    bool validate_tree = false;
+    cp.add_flag('v', "validate", validate_tree, "Validate the generated wavelet tree.");
+
+    std::string input_filename; // required
+    cp.add_param_string("file", input_filename, "The input file.");
+    if (!cp.process(argc, argv)) {
+        return -1;
+    }
+
+    // Init MPI
+    MPIContext ctx(&argc, &argv);
+
+    // start
+    switch(sym_width) {
+        case 1:
+            mpi_app_t::template start<uint8_t>(
+                ctx,
+                input_filename, prefix, rdbufsize, eff_input,
+                output);
+            if(validate_tree && ctx.is_master()) {
+                validate_distwt<uint8_t>(input_filename, output, ctx.num_workers());
+            }
+            return 0;
+
+        case 2:
+            mpi_app_t::template start<uint16_t>(
+                ctx,
+                input_filename, prefix, rdbufsize, eff_input,
+                output);
+            if(validate_tree && ctx.is_master()) {
+                validate_distwt<uint16_t>(input_filename, output, ctx.num_workers());
+            }
+            return 0;
+
+        case 4:
+            mpi_app_t::template start<uint32_t>(
+                ctx,
+                input_filename, prefix, rdbufsize, eff_input,
+                output);
+            if(validate_tree && ctx.is_master()) {
+                validate_distwt<uint32_t>(input_filename, output, ctx.num_workers());
+            }
+            return 0;
+
+        case 5:
+            mpi_app_t::template start<uint40_t>(
+                ctx,
+                input_filename, prefix, rdbufsize, eff_input,
+                output);
+            if(validate_tree && ctx.is_master()) {
+                ctx.cout_master() << "can not validate tree for 5 byte input symbol width\n";
+            }
+            return 0;
+
+        default:
+            ctx.cout_master()
+                << "symbol width of " << sym_width << " not supported"
+                << std::endl;
+            return -2;
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,6 @@
-#include "validate_distwt.hpp"
-#include <bitset>
 #include <distwt/apps/mpi_dd.hpp>
-#include <distwt/mpi/context.hpp>
-#include <distwt/mpi/uint_types.hpp>
-#include <fstream>
-#include <iostream>
-#include <iterator>
-#include <mpi.h>
-#include <string>
-#include <vector>
-
-void computeDist(int argc, char* argv[]) {
-    MPIContext ctx(&argc, &argv);
-    const std::string output("output/out");
-    const std::string input("test_input.txt");
-    mpi_dd::template start<uint8_t>(ctx, input, SIZE_MAX /* prefix */, 0 /* rdbufsize */,
-                                    false /* effective input (unused) */, output);
-    if (ctx.is_master()) {
-        validate_distwt<uint8_t>(input, output, ctx.num_workers());
-    }
-}
+#include <distwt/apps/mpi_launcher.hpp>
 
 int main(int argc, char* argv[]) {
-    computeDist(argc, argv);
-    return EXIT_SUCCESS;
+   return mpi_launch<mpi_dd>(argc, argv);
 }
